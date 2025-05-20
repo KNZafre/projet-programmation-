879f30701316fc97950f89ce2d21ae8234dc90db
KNZafre <kenza.hajhammami-e@ert.tn>
2025-05-20 11:02:27 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.showString("MEILLEUR PROF")
